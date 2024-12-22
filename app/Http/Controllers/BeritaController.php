1079a8916d939e3b2ec80c4ca32260dfce6478df
aufa19 <abdillahaufa79@gmail.com>
2024-12-22 22:09:07 +0700

    Sudah menambahkan controller, resource blade, dan routes utk Berita

--- app/Http/Controllers/BeritaController.php
+++ app/Http/Controllers/BeritaController.php
@@ -2,9 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Berita;
 use Illuminate\Http\Request;
 
 class BeritaController extends Controller
 {
-    //
+    public function index()
+    {
+        // Mengambil semua data dari model Berita
+        $beritas = Berita::paginate(9);
+
+        // Mengirimkan data ke view 'paket.index'
+        return view('berita', compact('beritas'));
+    }
 }
