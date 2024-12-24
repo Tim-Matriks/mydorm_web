4ac69ac1e69d87244a192b413535a1f369bccf85
zidan <zidansivafajar23@gmail.com>
2024-12-24 23:29:10 +0700

    Memperbarui seeder

--- database/seeders/HelpdeskSeeder.php
+++ database/seeders/HelpdeskSeeder.php
@@ -2,9 +2,10 @@
 
 namespace Database\Seeders;
 
-use App\Models\Helpdesk;
 use App\Models\Gedung;
+use App\Models\Helpdesk;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class HelpdeskSeeder extends Seeder
 {
@@ -12,12 +13,34 @@ class HelpdeskSeeder extends Seeder
     {
         // Ambil semua gedung
         $gedungs = Gedung::all();
+        $helpdeskData = [];
+        $index = 0;
+        $helpdesk = [
+            ["nama" => "Agus Prasetyo Santoso", "nip" => "202401010101000001"],
+            ["nama" => "Siti Kartika Putri", "nip" => "202401010102000002"],
+            ["nama" => "Rizky Permadi Wijaya", "nip" => "202401010103000003"],
+            ["nama" => "Dewi Anggraeni Sari", "nip" => "202401010104000004"],
+            ["nama" => "Ahmad Saputra Ramadhan", "nip" => "202401010105000005"],
+            ["nama" => "Fitri Wulandari Cahyani", "nip" => "202401010106000006"],
+            ["nama" => "Dimas Aditya Hartono", "nip" => "202401010107000007"],
+            ["nama" => "Ayu Melati Lestari", "nip" => "202401010108000008"]
+        ];
 
         // Untuk setiap gedung, buat 4 helpdesk
         foreach ($gedungs as $gedung) {
-            Helpdesk::factory()->count(4)->create([
-                'gedung_id' => $gedung->gedung_id,
-            ]);
+            for ($i = 0; $i < 4; $i++){
+                $helpdeskData[] = [
+                    'nama' => $helpdesk[$index]['nama'],
+                    'nip' => $helpdesk[$index]['nip'],
+                    'gedung_id' => $gedung->gedung_id,
+                    'created_at' => now(),
+                    'updated_at' => now()
+                ];
+    
+                $index++;
+            }   
         }
+
+        DB::table('helpdesk')->insert($helpdeskData);
     }
 }
