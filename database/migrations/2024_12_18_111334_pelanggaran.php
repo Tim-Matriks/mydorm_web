5930519ead57a828c74d2a934d855f54045dc5d6
aufa19 <abdillahaufa79@gmail.com>
2025-06-12 17:45:35 +0700

    add CI Test

--- database/migrations/2024_12_18_111334_pelanggaran.php
+++ database/migrations/2024_12_18_111334_pelanggaran.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id('pelanggaran_id');
             $table->string('kategori', 100);
             $table->dateTime('waktu');
-            $table->string('gambar', 255);
+            $table->string('gambar', 255)->nullable();
             $table->foreignId('senior_resident_id')->nullable()->constrained('senior_resident', 'senior_resident_id')->onDelete('set null');
             $table->foreignId('dormitizen_id')->nullable()->constrained('dormitizen', 'dormitizen_id')->onDelete('set null');
             $table->timestamps();
--- resources/views/pelanggaran/index.blade.php
+++ resources/views/pelanggaran/index.blade.php
@@ -6,7 +6,7 @@
             <form action="{{ route('pelanggaran.index') }}">
                 <div class="input-group">
                     <input type="text" name="search" class="form-control" placeholder="Cari disini"
-                        value="{{ request('search') ?? '' }}">
+                        value="{{ request('search', '') }}">
                     <button type="submit" class="btn btn-secondary">Cari</button>
                 </div>
             </form>
@@ -14,9 +14,9 @@
         <div class="col-md-3">
             <form action="{{ route('pelanggaran.index') }}" method="GET">
                 <select class="form-select" name="filter_sort" onchange="this.form.submit()">
-                    <option disabled @selected(!request('filter_sort'))>Urutkan</option>
-                    <option value="most"@selected(request('filter_sort') == 'most')>Most</option>
-                    <option value="least"@selected(request('filter_sort') == 'least')>Least</option>
+                    <option disabled {{ request('filter_sort') ? '' : 'selected' }}>Urutkan</option>
+                    <option value="most" {{ request('filter_sort') == 'most' ? 'selected' : '' }}>Most</option>
+                    <option value="least" {{ request('filter_sort') == 'least' ? 'selected' : '' }}>Least</option>
                 </select>
             </form>
         </div>
@@ -41,25 +41,25 @@
                             </thead>
                             <tbody>
                                 @foreach ($pelanggarans as $pelanggaran)
-                                    <tr>
-                                        <td>{{ $pelanggaran->dormitizen->kamar->nomor ?? 'N/A' }}</td>
-                                        <td>{{ $pelanggaran->dormitizen->nama ?? 'N/A' }}</td>
-                                        <td>
-                                            <div class="row">
-                                                <div class="progress col-6 p-0">
-                                                    <div class="progress progress-bar bg-danger" role="progressbar"
-                                                        style="width: {{ ($pelanggaran->total_pelanggaran / 9) * 100 }}%"
-                                                        aria-valuenow="{{ ($pelanggaran->total_pelanggaran / 9) * 100 }}"
-                                                        aria-valuemin="0" aria-valuemax="100"></div>
+                                <tr>
+                                    <td>{{ $pelanggaran->dormitizen->kamar->nomor}}</td>
+                                    <td>{{ $pelanggaran->dormitizen->nama}}</td>
+                                    <td>
+                                        <div class="row">
+                                            <div class="progress col-6 p-0">
+                                                <div class="progress progress-bar bg-danger" role="progressbar"
+                                                    style="width: {{ ($pelanggaran->total_pelanggaran / 9) * 100 }}%"
+                                                    aria-valuenow="{{ ($pelanggaran->total_pelanggaran / 9) * 100 }}">
                                                 </div>
-                                                <div class="col-6">{{ $pelanggaran->total_pelanggaran }}/9</div>
                                             </div>
-                                        </td>
-                                        <td>
-                                            <a href="{{ route('kamar.detail', [$pelanggaran->dormitizen->kamar->kamar_id, 'from' => 'pelanggaran']) }}"
-                                                class="btn btn-info">Detail</a>
-                                        </td>
-                                    </tr>
+                                            <div class="col-6">{{ $pelanggaran->total_pelanggaran }}/9</div>
+                                        </div>
+                                    </td>
+                                    <td>
+                                        <a href="{{ route('kamar.detail', [$pelanggaran->dormitizen->kamar->kamar_id, 'from' => 'pelanggaran']) }}"
+                                            class="btn btn-info">Detail</a>
+                                    </td>
+                                </tr>
                                 @endforeach
                             </tbody>
                         </table>
